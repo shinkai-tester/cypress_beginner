004556635475305a821bfd9d21bdf3748c08e47a
shinkai-tester <alexandra.klimantowa@gmail.com>
2024-06-11 02:56:19 +0200

    [HW_5_2_1] updated configs to run tests on Сypress dashboard

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "vsg4z7",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
