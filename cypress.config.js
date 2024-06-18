378da1506512f5b4b80ef502ae7d8160ae6ab917
Aleksandra Klimantova <alexandra.klimantowa@gmail.com>
2024-06-18 02:44:58 +0200

    [HW_5_2_2] added header_menu tests

--- cypress.config.js
+++ cypress.config.js
@@ -7,7 +7,7 @@ module.exports = defineConfig({
     baseUrl: "https://sqlverifier-live-6e21ca0ed768.herokuapp.com",
     env: {
       student_username: "shurka",
-      student_password: "123456",
+      student_password: "Test123456!",
     },
     setupNodeEvents(on, config) {
       // implement node event listeners here
--- cypress/e2e/verifier/header_menu.cy.js
+++ cypress/e2e/verifier/header_menu.cy.js
@@ -23,4 +23,94 @@ describe("Tests on redirection after clicking header's menu elements", () => {
     cy.url().should("include", "user-task");
     cy.get("#user-task-heading").should("contain.text", "User Tasks");
   });
+
+  it("Redirection to Swagger -> API", () => {
+    cy.get("#docs-menu [aria-expanded='false'] span").click();
+    cy.get("#docs-menu .dropdown-item span").click();
+    cy.url().should("include", "docs/docs");
+    cy.get("iframe[title='Swagger UI']").should("be.visible");
+  });
+
+  it("Switching to French", () => {
+    cy.get("#docs-menu + li span").click();
+    cy.get("button[value='fr']").click();
+    cy.get("#header-tabs").should(($el) => {
+      expect($el).to.contain.text("Accueil");
+      expect($el).to.contain.text("Entités");
+      expect($el).to.contain.text("Français");
+      expect($el).to.contain.text("Compte");
+    });
+    cy.url().should("include", "?page=1&sort=id,asc");
+  });
+
+  it("Switching to Russian", () => {
+    cy.get("#docs-menu + li span").click();
+    cy.get("button[value='ru']").click();
+    cy.get("#header-tabs").should(($el) => {
+      expect($el).to.contain.text("Главная");
+      expect($el).to.contain.text("Сущности");
+      expect($el).to.contain.text("Русский");
+      expect($el).to.contain.text("Профиль");
+    });
+    cy.url().should("include", "?page=1&sort=id,asc");
+  });
+
+  it("Switching to Ukrainian", () => {
+    cy.get("#docs-menu + li span").click();
+    cy.get("button[value='ua']").click();
+    cy.get("#header-tabs").should(($el) => {
+      expect($el).to.contain.text("Головна");
+      expect($el).to.contain.text("Сутності");
+      expect($el).to.contain.text("Українська");
+      expect($el).to.contain.text("Профіль");
+    });
+    cy.url().should("include", "?page=1&sort=id,asc");
+  });
+
+  it("Switching to English", () => {
+    cy.get("#docs-menu + li span").click();
+    cy.get("button[value='en']").click();
+    cy.get("#header-tabs").should(($el) => {
+      expect($el).to.contain.text("Home");
+      expect($el).to.contain.text("Entities");
+      expect($el).to.contain.text("English");
+      expect($el).to.contain.text("Account");
+    });
+    cy.url().should("include", "?page=1&sort=id,asc");
+  });
+
+  it("Redirection to Account Settings", () => {
+    cy.get("#account-menu").click();
+    cy.get(".dropdown-menu a[data-cy='settings']").click();
+    cy.get("#settings-title span").should("contain.text", "User settings");
+    cy.get("#settings-form").should("be.visible");
+    cy.url().should("include", "account/settings");
+  });
+
+  it("Redirection to Account Password menu item", () => {
+    cy.get("#account-menu").click();
+    cy.get(".dropdown-menu a[data-cy='passwordItem']").click();
+    cy.get("#password-title span").should("contain.text", "Password for");
+    cy.get("#password-form").should("be.visible");
+    cy.url().should("include", "account/password");
+  });
+
+  it("Signing out after clicking 'Sign out' button", () => {
+    cy.get("#account-menu").click();
+    cy.get(".dropdown-menu a[data-cy='logout']").click();
+    cy.get("#app-view-container h4").should(
+      "have.text",
+      "Logged out successfully!"
+    );
+    cy.url().should("include", "logout");
+  });
+
+  it("Redirection to Home page", () => {
+    cy.get("#account-menu").click();
+    cy.get(".dropdown-menu a[data-cy='settings']").click();
+    cy.url().should("include", "account/settings");
+    cy.get("#header-tabs li:first-child").click();
+    cy.url().should("include", "?page=1&sort=id,asc");
+    cy.get("#task-heading").should("be.visible");
+  });
 });
