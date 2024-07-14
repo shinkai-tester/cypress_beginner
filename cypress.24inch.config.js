670346cbd7fc9a6155076929741b509bf4f29e9d
Aleksandra Klimantova <alexandra.klimantowa@gmail.com>
2024-07-15 00:36:59 +0200

    [HW_9_1_1] added new configs, command for parallel test run

---
+++ cypress.24inch.config.js
@@ -0,0 +1,26 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  projectId: "gfwubd",
+  watchForFileChanges: false,
+  viewportWidth: 1920,
+  viewportHeight: 1080,
+  e2e: {
+    baseUrl: "https://sqlverifier-live-6e21ca0ed768.herokuapp.com",
+    env: {
+      configFile: "cypress.config.js",
+      student_username: "shurka",
+      student_password: "Test123456!",
+      admin: "admin_automation",
+      teacher_username: "shurka_teacher",
+      teacher_password: "Test1234!",
+    },
+    setupNodeEvents(on, config) {
+      on("before:spec", () => {
+        console.log(`Using config file: ${config.env.configFile}`);
+      });
+
+      return config;
+    },
+  },
+});
